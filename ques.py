baf17a3bc62c256ad16ec4fb8c88df3e9fa08b0b
divyanshukla-b <76198317+divyanshukla-b@users.noreply.github.com>
2022-10-19 13:39:31 +0530

    Update ques.py

--- ques.py
+++ ques.py
@@ -1,4 +1,4 @@
-class questionsclass:
-    def __init__(self, prompt, ans):
-        self.prompt = prompt
-        self.ans = ans
+# class questionsclass:
+#     def __init__(self, prompt, ans):
+#         self.prompt = prompt
+#         self.ans = ans
